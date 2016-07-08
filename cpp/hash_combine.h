3f3aeee91f0daa7fbb60bd65f55b01d2875e0226
boxdot <d@zerovolt.org>
2016-07-08 14:01:48 +0200

    Hash for tuple.

--- cpp/hash_combine.h
+++ cpp/hash_combine.h
@@ -1,4 +1,8 @@
 #include <functional>
+#include <string>
+#include <tuple>
+#include <utility>
+
 
 template <class T>
 inline void hash_combine(std::size_t& seed, const T& v)
@@ -6,3 +10,52 @@ inline void hash_combine(std::size_t& seed, const T& v)
     std::hash<T> hasher;
     seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
 }
+
+
+namespace {
+
+template <size_t n>
+struct tuple_hash {
+    template <typename... Types>
+    static void combine(std::size_t& seed, const std::tuple<Types...>& tpl) {
+        tuple_hash<n - 1>::combine(seed, tpl);
+        hash_combine(seed, std::get<n>(tpl));
+    }
+};
+
+template<>
+struct tuple_hash<0> {
+    template <typename... Types>
+    static void combine(std::size_t& seed, const std::tuple<Types...>& tpl) {
+        hash_combine(seed, std::get<0>(tpl));
+    }
+};
+
+} // namespace anonymous
+
+
+namespace std {
+
+// pair hash
+template <typename T1, typename T2>
+struct hash<std::pair<T1, T2>> {
+    auto operator()(const std::pair<T1, T2>& p) const {
+        size_t seed = 0;
+        hash_combine(seed, p.first);
+        hash_combine(seed, p.second);
+        return seed;
+    }
+};
+
+
+// tuple hash
+template<typename... Types>
+struct hash<std::tuple<Types...>> {
+    auto operator()(const std::tuple<Types...>& tpl) const {
+        size_t seed = 0;
+        tuple_hash<sizeof...(Types) - 1>::combine(seed, tpl);
+        return seed;
+    }
+};
+
+} // namespace std
--- cpp/tests/test_hash_combine.cpp
+++ cpp/tests/test_hash_combine.cpp
@@ -1,6 +1,7 @@
 #include "../hash_combine.h"
 #include <catch.hpp>
 #include <string>
+#include <unordered_map>
 
 
 TEST_CASE("Hash combine smoke test", "[hash_combine]")
@@ -10,4 +11,47 @@ TEST_CASE("Hash combine smoke test", "[hash_combine]")
     hash_combine(seed, 42.);
     hash_combine(seed, 42.f);
     hash_combine(seed, std::string("foobar"));
+}
+
+TEST_CASE("Pair hash smoke test", "[hash_combine]")
+{
+    std::unordered_map<std::pair<long, std::string>, int> mapping =
+        { {{0, "Hello"}, 5}
+        , {{1, "World"}, 5}
+        };
+}
+
+TEST_CASE("Pair hash test", "[hash_combine]")
+{
+    auto p = std::make_pair(1, std::string("Hello, World!"));
+    auto hash = std::hash<decltype(p)>{}(p);
+
+    size_t seed = 0;
+    hash_combine(seed, std::hash<int>{}(1));
+    hash_combine(seed, std::hash<std::string>{}(std::string("Hello, World!")));
+
+    REQUIRE(hash == seed);
+}
+
+
+TEST_CASE("Tuple hash smoke test", "[hash_combine]") {
+    std::unordered_map<std::tuple<long, long, long>, long> mapping =
+        { {{0, 1, 2}, 3}
+        , {{1, 2, 3}, 6}
+        , {{2, 3, 6}, 11}
+        };
+}
+
+TEST_CASE("Tuple hash test", "[hash_combine]")
+{
+    auto tpl = std::make_tuple(1, 2, std::string("Hello, World!"), 42.);
+    auto hash = std::hash<decltype(tpl)>{}(tpl);
+
+    size_t seed = 0;
+    hash_combine(seed, std::hash<int>{}(1));
+    hash_combine(seed, std::hash<int>{}(2));
+    hash_combine(seed, std::hash<std::string>{}(std::string("Hello, World!")));
+    hash_combine(seed, std::hash<double>{}(42.));
+
+    REQUIRE(hash == seed);
 }
